2fccdc59021d43b5dfc628360d54c965372c1115
Praveen Chaudhary <praveen.chaudhary@incred.com>
2021-10-01 17:56:34 +0530

    Build config push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true
 };
+
+export const configuration = {
+  companyName: 'Cool Dude',
+  facebook: 'https://facebook.com',
+  google: 'https://google.com',
+  instagram: 'https://instagram.com',
+  twitter: 'https://twitter.com'
+}
